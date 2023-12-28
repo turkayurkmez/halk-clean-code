4eb649444497ca006808a21033674bcfe9e57e63
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-12-28 12:25:42 +0300

    initialize repository and naming explained

---
+++ Naming/Naming/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+int i = 0;
+int d = 120; //Müşterinin kayıt olduğu tarihten bu yana geçen gün sayısı
+int day = 120;
+int daysSinceUserCreatedOnDb = 120;
+
+int kullaniciKacGundurKayitli = 120;
+
+//bool banuAlkan = false;
+
+var hitPoint = 100;
+
+//takım lideri karar alır....
+var cities = new List<string>();
+var cityList = new List<string>();
+
+string stringName = "Türkay";
+int count = 4;
+Random randomNumberGenerator = new Random();
+FileStream reportFileStream = null;
+DataTable customersDataTable = null;
+
+bool isCanceled = false;
+//if (isCanceled)
+//{
+
+//}
+//var o = 0;
+//var l = 1;
+
+//if (o==l)
+//{
+//    l = 1;
+//}
+
+List<int[]> cellsOnABoard = new List<int[]>();
+
+List<int[]> getFlaggedCellsOnGameBoard()
+{
+    List<int[]> flaggedCells = new List<int[]>();
+    foreach (var cell in cellsOnABoard)
+    {
+        if (cell[0] == 4)
+        {
+            flaggedCells.Add(cell);
+        }
+    }
+    return flaggedCells;
+}
+
+
